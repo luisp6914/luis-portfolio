2062f88cfc94cffdd33efd9119148a9dc01de0b1
Luis Perez <luisperez@Mac.lan>
2025-06-06 20:10:32 -0700

    Updated message on toast and added request to the end points in order to boot them up sooner

--- src/Home/components/HomePage.tsx
+++ src/Home/components/HomePage.tsx
@@ -7,15 +7,58 @@ import Skills from "./Skills";
 
 // @ts-ignore
 import Toast from "bootstrap/js/dist/toast.js";
+import axios from "axios";
 
 const HomePage = () => {
     const toastRef = useRef<HTMLDivElement>(null);
+
+    const allVaccinesURL = import.meta.env.VITE_ALL_VACCINES_URL;
+    const allPatientsURL = import.meta.env.VITE_ALL_PATIENTS_URL;
+    const userPicksURL = import.meta.env.VITE_USER_PICKS_URL;
+    //Getting list of vaccines 
+    const fetchVaccines = async () => {
+        try {
+            const response = await axios.get(allVaccinesURL);
+            if(response){
+                console.log("Fetched Vaccines")
+            }
+        } catch (error) {
+            console.error("Error getting Vaccines [HomePage.tsx]", error)
+        }
+    }
+
+    //Getting patients
+    const fetchPatients = async () => {
+        try {
+            const response = await axios.get(allPatientsURL);
+            if(response){
+                console.log("Fetched Patients");
+            }
+        } catch (error) {
+            console.error("Error fetching patients [HomePage.tsx]", error);
+        }
+    }
+
+    //Fetching the User picks
+    const fetchUserPicks = async () => {
+        try {
+            const response = await axios.get(userPicksURL);
+            if(response){
+                console.log("Fetched User Picks");
+            } 
+        } catch (error) {
+            console.error("Error fetching User Picks [HomePage.tsx]",error)
+        }
+    }
 
     useEffect(() => {
         if (toastRef.current) {
             const toast = new Toast(toastRef.current);
             toast.show();
         }
+        fetchPatients();
+        fetchUserPicks();
+        fetchVaccines();
     }, []);
 
     return (
@@ -23,7 +66,7 @@ const HomePage = () => {
             <div ref={toastRef} className="toast position-fixed bottom-0 end-0 p-3 mb-2 me-2 align-items-center text-white bg-primary border-0 auto-hide" role="alert" aria-live="assertive" aria-atomic="true">
                 <div className="d-flex">
                     <div className="toast-body">
-                        <b>Please note:</b> The Covid and PC Part Picker Project may load slowly initially as the Docker container service starts up. 
+                        <b>Please note:</b> The Covid and PC Part Picker Project may load slowly initially as the Render container service starts up. Usually around 3 minutes.
                     </div>
                     <button type="button" className="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
                 </div>
